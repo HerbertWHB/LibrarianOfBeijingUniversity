90364a21b370847364dd158250ffae2b54fc46f4
wanghaobo <wanghaobo@innover.com.cn>
2021-11-29 16:15:32 +0800

    first commit

---
+++ src/components/Homepage.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h3>{{Date()}}</h3>
+
+  <p>
+    Author: Herbert Wang
+  </p>
+
+  <p>
+   Vue3 with vite , quasar
+  </p>
+
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    quasar({
+      sassVariables: 'src/quasar-variables.sass'
+    })
+  ],
+  //添加sever端口
+  server:{
+    port:5050
+  }
+})
